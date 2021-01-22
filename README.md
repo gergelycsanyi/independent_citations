7cc368b8e291c7a602f56d26ab07209367f5dff6
Tamas Orosz <oroszt@montana.hu>
2021-01-22 18:18:29 +0100

    minimal refactoring in download_cited_papers.py

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Tool for collecting independent citations for scientists
 This tool can download the given authors paper from his/her google scholar profile. 
 Then downloads all of the citations and gives back a list with the independent citations. 
 Independent citations is calculated if the given authors name or id is not part of the citing papers author list. 
-Moreover, it is possible to define an exclusion list. s
+Moreover, it is possible to define an exclusion list, because in some cases for example a phd students article cannot be counted as an independent citation.
--- download_cited_papers.py
+++ download_cited_papers.py
@@ -19,17 +19,29 @@ filter_pat = re.compile(r"[A-ZÍŰÁÉÚŐÓÜÖ]{1,2}]")
 # pg.FreeProxies()
 # scholarly.use_proxy(pg)
 
-def save_json(name, pub):
+def save_json(title, publication):
+    """
+    Saves the citers of the downloaded publication into a json file.
+
+    :param title: title of the document 
+    :param publication: list of the collected citing publications
+    :return: 
+    """
     if not os.path.exists(DOWNLOAD_FOLDER):
         os.makedirs(DOWNLOAD_FOLDER)
 
-    with open(DOWNLOAD_FOLDER + "/" + name.replace(" ", "_") + ".json", "w", encoding="utf-8") as file:
-        json.dump(pub, file, ensure_ascii=False, indent=4)
+    with open(DOWNLOAD_FOLDER + "/" + title.replace(" ", "_") + ".json", "w", encoding="utf-8") as file:
+        json.dump(publication, file, ensure_ascii=False, indent=4)
 
 
-# Which papers cited that publication?
-# print([citation['bib']["author"] for citation in scholarly.citedby(pub)])  # ['title']
 def print_citations(res_dict, threshold=70):
+    """
+     Which papers cited that publication?
+     # print([citation['bib']["author"] for citation in scholarly.citedby(pub)])  # ['title']
+    :param res_dict:
+    :param threshold:
+    :return:
+    """
     author = res_dict["author"]
     citations = res_dict['cited_this']
     if isinstance(author, str):
@@ -55,39 +67,45 @@ def print_citations(res_dict, threshold=70):
     return independent_citations
 
 
-# Retrieve the author's data, fill-in, and print
-# search_query = scholarly.search_author(NAME)
-search_query = scholarly.search_author_id(AUTHOR_ID)
-# author = scholarly.fill(next(search_query))
-author = scholarly.fill(search_query)
-print(author)
+def download_citations():
+    # Retrieve the author's data, fill-in, and print
+    # search_query = scholarly.search_author(NAME)
+    search_query = scholarly.search_author_id(AUTHOR_ID)
+    # author = scholarly.fill(next(search_query))
+    author = scholarly.fill(search_query)
+    print(author)
 
-# Print the titles of the author's publications
-print([pub['bib']['title'] for pub in author['publications']])
+    # Print the titles of the author's publications
+    print([pub['bib']['title'] for pub in author['publications']])
 
-# Take a closer look at the first publication
-# pub = scholarly.fill(author['publications'][1])
-# print(pub)
-independent_citations = []
-for pub in author['publications'][17:]:
-    res_dict = {}
-    time.sleep(random.randint(WAIT, WAIT * 2))
-    pub = scholarly.fill(pub)
-    res_dict["title"] = pub['bib']["title"]
-    res_dict["year"] = pub['bib']["pub_year"]
-    print(pub['bib']["title"])
-    res_dict["author"] = [name.strip() for name in pub['bib']["author"].split("and")]
-    time.sleep(random.randint(WAIT, WAIT * 2))
-    cited_this = scholarly.citedby(pub)
-    if cited_this:
-        res_dict['cited_this'] = [{"author": citation['bib']["author"], "title": citation['bib']["title"]} for citation
-                                  in
-                                  cited_this]
-        indep_citations = print_citations(res_dict)
-        res_dict['independent_citations'] = indep_citations
-        independent_citations.append(
-            {"title": res_dict["title"], "author": res_dict["author"], 'independent_citations': indep_citations})
-        save_json(res_dict['title'], res_dict)
-    else:
-        break
-save_json("independent_citations.json", independent_citations)
+    # Take a closer look at the first publication
+    # pub = scholarly.fill(author['publications'][1])
+    # print(pub)
+    independent_citations = []
+    for pub in author['publications'][:]:
+        res_dict = {}
+        time.sleep(random.randint(WAIT, WAIT * 2))
+        pub = scholarly.fill(pub)
+        res_dict["title"] = pub['bib']["title"]
+        res_dict["year"] = pub['bib']["pub_year"]
+        print(pub['bib']["title"])
+        res_dict["author"] = [name.strip() for name in pub['bib']["author"].split("and")]
+        time.sleep(random.randint(WAIT, WAIT * 2))
+        cited_this = scholarly.citedby(pub)
+        if cited_this:
+            res_dict['cited_this'] = [{"author": citation['bib']["author"], "title": citation['bib']["title"]} for
+                                      citation
+                                      in
+                                      cited_this]
+            indep_citations = print_citations(res_dict)
+            res_dict['independent_citations'] = indep_citations
+            independent_citations.append(
+                {"title": res_dict["title"], "author": res_dict["author"], 'independent_citations': indep_citations})
+            save_json(res_dict['title'], res_dict)
+        else:
+            break
+
+    save_json("independent_citations.json", independent_citations)
+
+if __name__ == '__main__':
+    download_citations()
